8ae4dcc2abd728b104b64fd3e2be5f027ee6e5ec
hoang <dohoho.23.95@gmail.com>
2017-06-12 15:39:40 +0700

    code stability ver.6

---
+++ Terminal/define.h
@@ -0,0 +1,24 @@
+#ifndef DEFINE_H
+#define DEFINE_H
+
+#define IMAGES_PATH "/home/hoang/Desktop/WSAN/Images/Sensor"
+#define DATA_PATH "/home/hoang/Desktop/WSAN/dataHN.txt"
+#define GPS_FILE "/home/hoang/Desktop/WSAN/gps.txt"
+#define HISTORY_FILE "/home/hoang/Desktop/WSAN/history.txt"
+#define LOG_FILE "/home/hoang/Desktop/WSAN/log.txt"
+#define TIME_DETECT "/home/hoang/Desktop/WSAN/timeDetect.txt"
+
+#define NOFFUNC 3
+#define NOSENSOR 5
+
+#define TAKE_TEMP_AND_HUM 0
+#define TAKE_PHOTO 1
+#define REQUEST_STATUS 2
+#define RETASK 3
+
+#define TRANCEIVER_PORT_DEFAULT "ttyUSB0"
+#define TRANCEIVER_BAUDRATE_DEFAULT B19200
+#define GPS_PORT_DEFAULT "ttyUSB1"
+#define GPS_BAUDRATE_DEFAULT B9600
+
+#endif // DEFINE_H
